7e52896409b44ce2f6648210bd0514db486f2805
IrynaKolh <iryna.kolhanova@gmail.com>
2022-01-08 17:05:15 -0500

    reverse-int implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let i = 0;
+    let result = '';
+    n = n.toString();
+    while (i < n.length) {
+        result = `${n[i]}${result}`
+        i = i + 1;
+    }
+    return parseInt(result);
 }
